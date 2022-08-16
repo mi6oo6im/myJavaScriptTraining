d888d6d2ab3d6d955b0d8747dfebe958f50040c5
Mihail Istiliyanov <54684159+mi6oo6im@users.noreply.github.com>
2022-08-16 20:30:52 +0100

    Create 9numbersFromNTo1.js

---
+++ fundamentals/trainingBasicSyntax/9numbersFromNTo1.js
@@ -0,0 +1,9 @@
+function numbersFromNTo1(startNum) {
+    currentNumber = startNum
+    while (currentNumber >= 1) {
+        console.log(currentNumber);
+        currentNumber--
+    }
+}
+numbersFromNTo1(5);
+numbersFromNTo1(3);
