0441e4f9befc91e8308bfb13a929f52ae29a7ad0
Mihail Istiliyanov <54684159+mi6oo6im@users.noreply.github.com>
2022-08-19 21:04:01 +0100

    Create 1echoType.js

---
+++ fundamentals/2dataTypes&Variables/1echoType.js
@@ -0,0 +1,12 @@
+function echoType(param) {
+    type = typeof(param);
+    console.log(type);
+    if (type === 'string' || type === 'number'){
+        console.log(param);
+    } else {
+        console.log('Parameter is not suitable for printing');
+    }
+}
+echoType('Hello, JavaScript!');
+echoType(18);
+echoType(null);
