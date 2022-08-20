0c957c4252e28ed3106c00fbbef9f97e06c111a4
Mihail Istiliyanov <54684159+mi6oo6im@users.noreply.github.com>
2022-08-20 18:13:04 +0100

    Create 4biggestOf3Numbers.js

---
+++ fundamentals/2moreExerciseDataTypes&Variables/4biggestOf3Numbers.js
@@ -0,0 +1,8 @@
+function biggestOf3Numbers(num1, num2, num3) {
+    maxNumber = Math.max(num1, num2, num3)
+    console.log(maxNumber);
+}
+biggestOf3Numbers(-2, 7, 3);
+biggestOf3Numbers(130, 5, 99);
+biggestOf3Numbers(43, 43.2, 43.1);
+biggestOf3Numbers(2, 2, 2);
