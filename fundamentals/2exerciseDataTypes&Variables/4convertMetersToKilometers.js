5c7291dbe3728c547a478b43416773dadb1baf65
Mihail Istiliyanov <54684159+mi6oo6im@users.noreply.github.com>
2022-08-20 14:31:03 +0100

    Create 4convertMetersToKilometers.js

---
+++ fundamentals/2exerciseDataTypes&Variables/4convertMetersToKilometers.js
@@ -0,0 +1,6 @@
+function convertMetersToKilometers(meters) {
+    kilometers = meters / 1000;
+    console.log(kilometers.toFixed(2));
+}
+convertMetersToKilometers(1852);
+convertMetersToKilometers(798);
