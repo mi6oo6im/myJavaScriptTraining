f797a20ff2e8e24e9654dc9f4bc640fbb0524adf
Mihail Istiliyanov <54684159+mi6oo6im@users.noreply.github.com>
2022-10-15 18:33:47 +0100

    Create 1smalestOfThreeNumbers.js

---
+++ fundamentals/4exerciseFunctions/1smalestOfThreeNumbers.js
@@ -0,0 +1,11 @@
+function smalestOfThreeNumbers(x, y, z) {
+    console.log(Math.min(x, y, z));
+}
+smalestOfThreeNumbers(2, 5, 3);
+console.log('----------------');
+smalestOfThreeNumbers(600, 342, 123);
+console.log('----------------');
+smalestOfThreeNumbers(25, 21, 4);
+console.log('----------------');
+smalestOfThreeNumbers(2, 2, 2);
+console.log('----------------');
