30d55b3586696ff99537f92bdc037aa4b28b3246
Mihail Istiliyanov <54684159+mi6oo6im@users.noreply.github.com>
2022-10-27 21:01:16 +0100

    Create 1personInfo.js

---
+++ fundamentals/6trainingObjectsAndClasses/1personInfo.js
@@ -0,0 +1,18 @@
+function personInfo(firstName, lastName, age) {
+    let person = {
+        firstName,
+        lastName,
+        age,
+    }
+    return person
+}
+personInfo("Peter", 
+"Pan",
+"20"
+);
+console.log('--------------------');
+personInfo("George", 
+"Smith",
+"18"
+);
+console.log('--------------------');
