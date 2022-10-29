f972be39551274f03682ba86ff412e5da93a154b
Mihail Istiliyanov <54684159+mi6oo6im@users.noreply.github.com>
2022-10-29 19:05:37 +0100

    Create 6.makeADictionary.js

---
+++ fundamentals/6exerciseObjectsAndClasses/6.makeADictionary.js
@@ -0,0 +1,42 @@
+function makeADictionary(inputArr) {
+    let dictionaryArr = [];
+    inputArr.forEach(topic => {
+        let currentTopic = JSON.parse(topic);
+        let currentTerm = Object.keys(currentTopic)[0];
+        let currentDefinition = Object.values(currentTopic)[0];
+        let isPresent = false
+        dictionaryArr.forEach(term => {
+            if (term.hasOwnProperty(currentTerm)) {
+                term[currentTerm] = currentDefinition
+                isPresent = true;
+            }
+        });
+        if (!isPresent) {
+            dictionaryArr.push(currentTopic);
+        }
+    });
+    dictionaryArr.sort((a, b) => Object.keys(a)[0].localeCompare(Object.keys(b)));
+    dictionaryArr.forEach(topic => {
+        let [term, definition] = Object.entries(topic)[0];
+        console.log(`Term: ${term} => Definition: ${definition}`);
+    });
+}
+makeADictionary([
+    '{"Coffee":"A hot drink made from the roasted and ground seeds (coffee beans) of a tropical shrub."}',
+    '{"Bus":"A large motor vehicle carrying passengers by road, typically one serving the public on a fixed route and for a fare."}',
+    '{"Boiler":"A fuel-burning apparatus or container for heating water."}',
+    '{"Tape":"A narrow strip of material, typically used to hold or fasten something."}',
+    '{"Boiler":"test"}',
+    '{"Microphone":"An instrument for converting sound waves into electrical energy variations which may then be amplified, transmitted, or recorded."}'
+]
+)
+console.log('----------------------------------------------------------------------------------');
+makeADictionary([
+    '{"Cup":"A small bowl-shaped container for drinking from, typically having a handle"}',
+    '{"Cake":"An item of soft sweet food made from a mixture of flour, fat, eggs, sugar, and other ingredients, baked and sometimes iced or decorated."} ',
+    '{"Watermelon":"The large fruit of a plant of the gourd family, with smooth green skin, red pulp, and watery juice."} ',
+    '{"Music":"Vocal or instrumental sounds (or both) combined in such a way as to produce beauty of form, harmony, and expression of emotion."} ',
+    '{"Art":"The expression or application of human creative skill and imagination, typically in a visual form such as painting or sculpture, producing works to be appreciated primarily for their beauty or emotional power."} '
+]
+)
+console.log('----------------------------------------------------------------------------------');
