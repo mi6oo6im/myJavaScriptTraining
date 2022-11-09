ed2ae4f0fbfbae6333edd4debd498242b5fa700f
Mihail Istiliyanov <54684159+mi6oo6im@users.noreply.github.com>
2022-11-09 18:22:33 +0000

    Create 1printCharacters.js

---
+++ fundamentals/8trainingTextProcessing/1printCharacters.js
@@ -0,0 +1,9 @@
+function printCharacters(inputString) {
+    for (const letter of inputString) {
+        console.log(letter);
+    }
+}
+printCharacters('AWord');
+console.log('---------------------------');
+printCharacters('Sentence');
+console.log('---------------------------');
