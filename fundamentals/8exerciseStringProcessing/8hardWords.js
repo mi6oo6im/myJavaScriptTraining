ea945c90cfe285127ae62a763ef3192d82c79769
Mihail Istiliyanov <54684159+mi6oo6im@users.noreply.github.com>
2022-11-12 10:17:17 +0000

    Create 8hardWords.js

---
+++ fundamentals/8exerciseStringProcessing/8hardWords.js
@@ -0,0 +1,11 @@
+function hardWords(inputArr) {
+    const wordsSorted = inputArr.pop().sort((a, b) => b.length - a.length);
+    let text = String(inputArr);
+    wordsSorted.forEach(word => {
+        text = text.replace('_'.repeat(word.length), word);
+    });
+    console.log(text);
+}
+hardWords(['Hi, grandma! I\'m so ____ to write to you. ______ the winter vacation, so _______ things happened. My dad bought me a sled. Mom started a new job as a __________. My brother\'s ankle is ________, and now it bothers me even more. Every night Mom cooks ___ on your recipe because it is the most delicious. I hope this year Santa will _____ me a robot.',
+['pie', 'bring', 'glad', 'During', 'amazing', 'pharmacist', 'sprained']]
+);
